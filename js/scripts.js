8542c9043f59c389f82ff1d4e16494f9accc4799
Jamie Aquino <jamie.aquino33@gmail.com>
2019-09-24 10:45:01 -0500

    Functionality to add new list items and mark them as complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,59 @@
 
+/*
+ *Make references to the HTML elements that we want to interact with through JS
+ *Add in the ability to add a new list item
+ *Add in th ebaility to removes a list item
+ *Add in the ability to mark a list item as done
+ */
 
-console.log("TEST");
+ var submitButton = document.getElementById("submit");
+ var input = document.getElementById("userInput");
+ var ul = document.querySelector("ul"); //querySelector gets the first instance of a particular element type. Since we                                              only have one ul, this will create a reference to that ul.
+
+ function createListElement() {
+     //Create a new li element
+     var li = document.createElement("li");
+     //Add content to it
+     li.innerHTML = input.value;
+     //Add it to our ul
+     ul.appendChild(li);
+     //Clear out the text unput after we create our new list item
+     input.value = "";
+
+
+     //Funcition to handle showing a list item as complete
+     
+     function markDone() {
+        //If the li does not have a class "done" applied to it, it will be applied. If it does, it will be taken off.
+        li.classList.toggle('done');
+     }
+     li.addEventListener("click", markDone);
+ }
+
+ //This is going to be used to create a list element when the submit button is tapped
+ function createListItemSubmitButtom() {
+     //Check to make sure our input has some text. An empty string will have a length of 0
+     if (input.value.length > 0) {
+    createListElement();
+     }
+ }
+
+ //This is going to be used to create a list element when they enter key is pressed
+ function createListItemEnterKey(event) {
+     console.log(event.keyCode);
+
+     //Create a new list item if there is text in the unput field and the enter key is pressed
+     if (input.value.length > 0 && event.keyCode === 13) {
+         createListElement();
+     }
+
+ }
+
+
+ //Make it so when the submit button is clicked, the createListElement function is called
+ submitButton.addEventListener("click", createListItemSubmitButtom);
+
+ //This event listener will detect when keys are pressed while the input is active
+ input.addEventListener("keypress", createListItemEnterKey);
+
+ 
